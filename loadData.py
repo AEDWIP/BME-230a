dc9854928e14bce68ab6463a18fc3f0dea3b1bc8
andy@santaCruzIntegration.com <andy@SantaCruzIntegration.com>
2019-03-13 22:52:43 +0000

    early check in , able generate onehot examples

--- loadData.py
+++ loadData.py
@@ -10,7 +10,7 @@ public functions:
     saveKerasModel(modelName)
 '''
 
-
+from keras.utils import np_utils
 import numpy as np
 import os
 import pandas as pd
@@ -98,76 +98,76 @@ _tumorNormalFile = "data/logisticRegressionTumorNormal.h5"
 
 
 ################################################################################        
-def _AEDWIP_loadCleanWriteTumorNormalData():
-    '''
-    this does not work, loosing to much time debugging H5
+# def _AEDWIP_loadCleanWriteTumorNormalData():
+#     '''
+#     this does not work, loosing to much time debugging H5
     
-    1. loads data/tcga_target_gtex.h5
-    2. encodes Y["tumor_normal_value"]
-    3. StratifiedShuffleSplit on Y["tumor_normal_value"]
-    4. save to data/tumorNormal.h5
-    '''
-    store = pd.HDFStore(_sourceDataFile, mode="r")
-    print("{} source store.info():{}".format(_sourceDataFile, store.info()))
-    print("{} store.keys():{}".format(_sourceDataFile, store.keys()))
+#     1. loads data/tcga_target_gtex.h5
+#     2. encodes Y["tumor_normal_value"]
+#     3. StratifiedShuffleSplit on Y["tumor_normal_value"]
+#     4. save to data/tumorNormal.h5
+#     '''
+#     store = pd.HDFStore(_sourceDataFile, mode="r")
+#     print("{} source store.info():{}".format(_sourceDataFile, store.info()))
+#     print("{} store.keys():{}".format(_sourceDataFile, store.keys()))
     
-    # Load training set
-    X = pd.read_hdf(_sourceDataFile, "expression")
-    Y = pd.read_hdf(_sourceDataFile, "labels")
+#     # Load training set
+#     X = pd.read_hdf(_sourceDataFile, "expression")
+#     Y = pd.read_hdf(_sourceDataFile, "labels")
 
-    # Convert tumor_normal  into numerical values 
-    from sklearn.preprocessing import LabelEncoder
+#     # Convert tumor_normal  into numerical values 
+#     from sklearn.preprocessing import LabelEncoder
     
-    encoder = LabelEncoder()
-    Y["tumor_normal_value"] = pd.Series(encoder.fit_transform(Y["tumor_normal"]), index=Y.index)
-    #Y[["tumor_normal","tumor_normal_value"]].head(3)
+#     encoder = LabelEncoder()
+#     Y["tumor_normal_value"] = pd.Series(encoder.fit_transform(Y["tumor_normal"]), index=Y.index)
+#     #Y[["tumor_normal","tumor_normal_value"]].head(3)
     
-    # Split into stratified training and test sets based on classes (i.e. tissue type) so that we have equal
-    # proportions of each tissue type in the train and test sets
-    from sklearn.model_selection import StratifiedShuffleSplit
-    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=theMeaningOfLife)
-    for train_index, test_index in split.split(X.values, Y["tumor_normal_value"]):
-        X_train, X_test = X.values[train_index], X.values[test_index]
-        y_train, y_test = Y["tumor_normal_value"][train_index], \
-                            Y["tumor_normal_value"][test_index]  
+#     # Split into stratified training and test sets based on classes (i.e. tissue type) so that we have equal
+#     # proportions of each tissue type in the train and test sets
+#     from sklearn.model_selection import StratifiedShuffleSplit
+#     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=theMeaningOfLife)
+#     for train_index, test_index in split.split(X.values, Y["tumor_normal_value"]):
+#         X_train, X_test = X.values[train_index], X.values[test_index]
+#         y_train, y_test = Y["tumor_normal_value"][train_index], \
+#                             Y["tumor_normal_value"][test_index]  
                             
-    print("AEDWIP type(X_train):{}".format(type(X_train)))
-    print("AEDWIP type(y_train):{}".format(type(y_train)))                             
-    print("AEDWIP X_train.shape:{}".format(X_train.shape))
-    print("AEDWIP y_train.shape:{}".format(y_train.shape)) 
-    sys.stdout.flush() 
+#     print("AEDWIP type(X_train):{}".format(type(X_train)))
+#     print("AEDWIP type(y_train):{}".format(type(y_train)))                             
+#     print("AEDWIP X_train.shape:{}".format(X_train.shape))
+#     print("AEDWIP y_train.shape:{}".format(y_train.shape)) 
+#     sys.stdout.flush() 
     
-    X_train = pd.DataFrame(data=X_train, columns=X.columns)
-    y_train = pd.DataFrame(data=y_train, columns=["tumor_normal_value"])
-    X_test  = pd.DataFrame(data=X_test,  columns=X.columns)
-    y_test  = pd.DataFrame(data=y_test,  columns=["tumor_normal_value"])
+#     X_train = pd.DataFrame(data=X_train, columns=X.columns)
+#     y_train = pd.DataFrame(data=y_train, columns=["tumor_normal_value"])
+#     X_test  = pd.DataFrame(data=X_test,  columns=X.columns)
+#     y_test  = pd.DataFrame(data=y_test,  columns=["tumor_normal_value"])
     
-    print("\n convert to pandas")
-    print("AEDWIP type(X_train):{}".format(type(X_train)))
-    print("AEDWIP type(y_train):{}".format(type(y_train)))                             
-    print("AEDWIP X_train.shape:{}".format(X_train.shape))
-    print("AEDWIP y_train.shape:{}".format(y_train.shape)) 
-    sys.stdout.flush()     
+#     print("\n convert to pandas")
+#     print("AEDWIP type(X_train):{}".format(type(X_train)))
+#     print("AEDWIP type(y_train):{}".format(type(y_train)))                             
+#     print("AEDWIP X_train.shape:{}".format(X_train.shape))
+#     print("AEDWIP y_train.shape:{}".format(y_train.shape)) 
+#     sys.stdout.flush()     
     
-    # numpy.ndarray' object has no attribute 'dtypes'                      
-#     print("AEDWIP X_train.dtypes:{}".format(X_train.dtypes))
-#     print("AEDWIP y_train.dtypes:{}".format(y_train.dtypes))
-    # Export h5 format files
-    with pd.HDFStore(_tumorNormalFile, "w") as store:
-        # https://riptutorial.com/pandas/example/9812/using-hdfstore
-        store.append("X_train", X_train)
-        print("stored XT")
-        store.append("y_train", y_train)
-        print("stored yt")
-        store.append("X_test",  X_test)
-        print ("stored xtest")
-        store.append("y_test",  y_test)
-        print("stored ytest")
-        # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.T.html
-#         store["X_train"] = X_train #.T.sort_index(axis="columns")
-#         store["y_train"] = y_train        
-#         store["X_test"] = X_test #.T.sort_index(axis="columns")
-#         store["y_test"] = y_test
+#     # numpy.ndarray' object has no attribute 'dtypes'                      
+# #     print("AEDWIP X_train.dtypes:{}".format(X_train.dtypes))
+# #     print("AEDWIP y_train.dtypes:{}".format(y_train.dtypes))
+#     # Export h5 format files
+#     with pd.HDFStore(_tumorNormalFile, "w") as store:
+#         # https://riptutorial.com/pandas/example/9812/using-hdfstore
+#         store.append("X_train", X_train)
+#         print("stored XT")
+#         store.append("y_train", y_train)
+#         print("stored yt")
+#         store.append("X_test",  X_test)
+#         print ("stored xtest")
+#         store.append("y_test",  y_test)
+#         print("stored ytest")
+#         # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.T.html
+# #         store["X_train"] = X_train #.T.sort_index(axis="columns")
+# #         store["y_train"] = y_train        
+# #         store["X_test"] = X_test #.T.sort_index(axis="columns")
+# #         store["y_test"] = y_test
 
 # ################################################################################        
 # def loadTumorNormalData():
@@ -242,6 +242,62 @@ def loadTumorNormalData(projectDir):
     #return (XTrainDF, yTrainDF, XTestDF, yTestDF)
     return (XTrainDF, y_train, XTestDF, y_test)
 
+################################################################################        
+def loadDiseaseTypeData(projectDir):
+    '''
+    ref: diseaseTypeClassifier.ipynb
+    
+    return (XTrainNumpy, yTrainNumpy, XTestNumpy, yTestNumpy)
+        note the shape of y will be (m, 1 + k). The first col is the disease category value, 
+        the remaining columns are the equavalent one hot encoding
+    '''
+    sourceDataFilePath = "{}/{}".format(projectDir, _sourceDataFile)
+    print("sourceDataFilePath:{}".format(sourceDataFilePath))
+    store = pd.HDFStore(sourceDataFilePath, mode="r")
+    #print("{} source store.info():{}".format(_sourceDataFile, store.info()))
+    #print("{} store.keys():{}".format(_sourceDataFile, store.keys()))
+     
+    # Load training set
+    XDF = pd.read_hdf(sourceDataFilePath, "expression")
+    yDF = pd.read_hdf(sourceDataFilePath, "labels")
+ 
+    # Convert tumor_normal  into numerical values 
+    from sklearn.preprocessing import LabelEncoder
+    
+    # Convert disease  into numerical values 
+    encoder = LabelEncoder()
+    yDF["disease_value"] = pd.Series(encoder.fit_transform(yDF["disease"]), index=yDF.index)
+    
+    # create one hot encodings
+    oneHots = np_utils.to_categorical(yDF["disease_value"])
+    #print(type(oneHots))
+    #print(oneHots.shape)
+
+    diseaseValues = yDF["disease_value"].values.reshape(yDF.shape[0],-1)
+    #print(type(diseaseValues))
+    #print(diseaseValues.shape)
+
+    yOneHots = np.append(diseaseValues, oneHots, axis=1)
+    
+    # Split into stratified training and test sets based on classes (i.e. disease type) 
+    # so that we have equal proportions of each disease type in the train and test sets
+  
+    from sklearn.model_selection import StratifiedShuffleSplit
+    split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=theMeaningOfLife)
+    for train_index, test_index in split.split(XDF.values, yDF["disease_value"]):
+        XTrainNumpy = XDF.values[train_index]
+        XTestNumpy  =  XDF.values[test_index]
+        yTrainNumpy = yOneHots[train_index]
+        yTestNumpy  = yOneHots[test_index]
+        
+    # pandas series values attribute create numpy arrays with shapes that are under specified
+    # E.G. yTestNumpy.shape: (3826,) .this causes a lot of bugs in other packages
+    # reshape(n,-1) causes reshape to set the last value
+    yTrainNumpy = np.reshape(yTrainNumpy,(yTrainNumpy.shape[0], -1))
+    yTestNumpy  = np.reshape( yTestNumpy, (yTestNumpy.shape[0], -1))   
+    
+    return (XTrainNumpy, yTrainNumpy, XTestNumpy, yTestNumpy)
+    
 ################################################################################        
 def loadKerasModel(modelName):
     '''
