135a10038d7de1634e32c51dc8354d2cbe99efd2
Andy Davidson <andy_davidson@apple.com>
2019-03-24 12:30:52 -0700

    # fixed gitHub security warning   - PyYAML 3.13 was upgrade to 5.15
    ## added autopep8

--- loadData.py
+++ loadData.py
@@ -20,7 +20,6 @@ import sys
 theMeaningOfLife = 42
 np.random.seed(theMeaningOfLife)
 
-
 _sourceDataFile = "data/tcga_target_gtex.h5"            
 _tumorNormalFile = "data/logisticRegressionTumorNormal.h5" 
 
@@ -38,8 +37,8 @@ def loadTumorNormalData(projectDir):
     sourceDataFilePath = "{}/{}".format(projectDir, _sourceDataFile)
     print("sourceDataFilePath:{}".format(sourceDataFilePath))
     store = pd.HDFStore(sourceDataFilePath, mode="r")
-    #print("{} source store.info():{}".format(_sourceDataFile, store.info()))
-    #print("{} store.keys():{}".format(_sourceDataFile, store.keys()))
+    # print("{} source store.info():{}".format(_sourceDataFile, store.info()))
+    # print("{} store.keys():{}".format(_sourceDataFile, store.keys()))
      
     # Load training set
     X = pd.read_hdf(sourceDataFilePath, "expression")
@@ -50,7 +49,7 @@ def loadTumorNormalData(projectDir):
      
     encoder = LabelEncoder()
     Y["tumor_normal_value"] = pd.Series(encoder.fit_transform(Y["tumor_normal"]), index=Y.index)
-    #Y[["tumor_normal","tumor_normal_value"]].head(3)
+    # Y[["tumor_normal","tumor_normal_value"]].head(3)
      
     # Split into stratified training and test sets based on classes (i.e. tissue type) so that we have equal
     # proportions of each tissue type in the train and test sets
@@ -61,21 +60,22 @@ def loadTumorNormalData(projectDir):
         y_train, y_test = Y["tumor_normal_value"][train_index], \
                             Y["tumor_normal_value"][test_index]  
      
-    #print("AEDWIP X_train.shape:{}".format(X_train.shape))
-    #print("AEDWIP X.shape:{} len(X.columns):{}".format(X.shape, len(X.columns)))
+    # print("AEDWIP X_train.shape:{}".format(X_train.shape))
+    # print("AEDWIP X.shape:{} len(X.columns):{}".format(X.shape, len(X.columns)))
     
     # X_train and X_test are numpy arrays
     XTrainDF = pd.DataFrame(X_train, columns=X.columns)
     XTestDF = pd.DataFrame(X_test, columns=X.columns)
     
     # y_train and y_test are pandas series
-    #yTrainDF = pd.DataFrame(y_train, columns=["tumor_normal_value"])
+    # yTrainDF = pd.DataFrame(y_train, columns=["tumor_normal_value"])
     yTrainDF = pd.DataFrame(y_train, columns=["tumor_normal_value"])
     yTrainDF.head(3)
     yTestDF = pd.DataFrame(y_test, columns=["tumor_normal_value"])
-    #return (X_train, y_train, X_test, y_test)
-    #return (XTrainDF, yTrainDF, XTestDF, yTestDF)
+    # return (X_train, y_train, X_test, y_test)
+    # return (XTrainDF, yTrainDF, XTestDF, yTestDF)
     return (XTrainDF, y_train, XTestDF, y_test)
+
 
 ################################################################################        
 def loadCancerDiseaseTypeTidyDataSet(projectDir):
@@ -111,8 +111,8 @@ def loadCancerDiseaseTypeTidyDataSet(projectDir):
     sourceDataFilePath = "{}/{}".format(projectDir, _sourceDataFile)
     print("sourceDataFilePath:{}".format(sourceDataFilePath))
     store = pd.HDFStore(sourceDataFilePath, mode="r")
-    #print("{} source store.info():{}".format(_sourceDataFile, store.info()))
-    #print("{} store.keys():{}".format(_sourceDataFile, store.keys()))
+    # print("{} source store.info():{}".format(_sourceDataFile, store.info()))
+    # print("{} store.keys():{}".format(_sourceDataFile, store.keys()))
      
     # Load training set
     XDF = pd.read_hdf(sourceDataFilePath, "expression")
@@ -127,11 +127,11 @@ def loadCancerDiseaseTypeTidyDataSet(projectDir):
 
     yTumorDF = yDF[yTumorRows]
     yDF = yTumorDF
-    yTumorDF = None # free memory
+    yTumorDF = None  # free memory
 
     XTumorDF = XDF[yTumorRows]
     XDF = XTumorDF
-    XTumorDF = None # free memory
+    XTumorDF = None  # free memory
 
     # Convert tumor_normal  into numerical values 
     from sklearn.preprocessing import LabelEncoder
@@ -143,12 +143,12 @@ def loadCancerDiseaseTypeTidyDataSet(projectDir):
     
     # create one hot encodings
     oneHots = np_utils.to_categorical(yDF["disease_value"])
-    #print(type(oneHots))
-    #print(oneHots.shape)
+    # print(type(oneHots))
+    # print(oneHots.shape)
 
-    diseaseValues = yDF["disease_value"].values.reshape(yDF.shape[0],-1)
-    #print(type(diseaseValues))
-    #print(diseaseValues.shape)
+    diseaseValues = yDF["disease_value"].values.reshape(yDF.shape[0], -1)
+    # print(type(diseaseValues))
+    # print(diseaseValues.shape)
 
     yOneHots = np.append(diseaseValues, oneHots, axis=1)
     
@@ -159,16 +159,15 @@ def loadCancerDiseaseTypeTidyDataSet(projectDir):
     split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=theMeaningOfLife)
     for train_index, test_index in split.split(XDF.values, yDF["disease_value"]):
         XTrainNumpy = XDF.values[train_index]
-        XTestNumpy  =  XDF.values[test_index]
+        XTestNumpy = XDF.values[test_index]
         yTrainNumpy = yOneHots[train_index]
-        yTestNumpy  = yOneHots[test_index]
+        yTestNumpy = yOneHots[test_index]
         
     # pandas series values attribute create numpy arrays with shapes that are under specified
     # E.G. yTestNumpy.shape: (3826,) .this causes a lot of bugs in other packages
     # reshape(n,-1) causes reshape to set the last value
-    yTrainNumpy = np.reshape(yTrainNumpy,(yTrainNumpy.shape[0], -1))
-    yTestNumpy  = np.reshape( yTestNumpy, (yTestNumpy.shape[0], -1))   
+    yTrainNumpy = np.reshape(yTrainNumpy, (yTrainNumpy.shape[0], -1))
+    yTestNumpy = np.reshape(yTestNumpy, (yTestNumpy.shape[0], -1))   
     
     return (hugoIds, diseaseLabelEncoder, XTrainNumpy, yTrainNumpy, XTestNumpy, yTestNumpy)
-    
 
